2749c0c6cf21969d00d510febb051c4a96500316
romaten1 <romaten1@rambler.ru>
2015-04-15 12:48:02 +0300

    first commit - install yii2

---
+++ views/layouts/_footer.php
@@ -0,0 +1,82 @@
+<div class="footer">
+    <div class="UA-brand">
+        <div class="wrapper">
+            <ul class="UA-brand-list">
+                <li><a href="#" title="ЕтноМодерн"><img src="img/ethno.png" alt="ЕтноМодерн"></a></li>
+                <li><a href="#" title="nenka"><img src="img/nenka.png" alt="nenka"></a></li>
+                <li><a href="#" title="raslov"><img src="img/raslov.png" alt="raslov"></a></li>
+                <li><a href="#" title="sem"><img src="img/sem.png" alt="sem"></a></li>
+                <li><a href="#" title="tago"><img src="img/tago.png" alt="tago"></a></li>
+                <li><a href="#" title="corali"><img src="img/corali.jpg" alt="corali"></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="footer-container">
+        <div class="wrapper">
+            <div class="footer-lists">
+                <div class="footer-column column-1">
+                    <span class="list-head">Про нас</span>
+                    <ul>
+                        <li><a href="#" title="Що таке UA КАТАЛОГ?">Що таке UA КАТАЛОГ?</a></li>
+                        <li><a href="#" title="Реквізити">Реквізити</a></li>
+                        <li><a href="#" title="Робота і вакансії">Робота і вакансії</a></li>
+                        <li><a href="#" title="Контакти">Контакти</a></li>
+                    </ul>
+                    <span class="social-head">бУДЬТЕ НА ЗВ’ЯЗКУ</span>
+                    <ul class="social-list">
+                        <li><a href="#" title="" class="fb"> </a></li><!--
+    			    		--><li><a href="#" title="" class="vk"> </a></li><!--
+    			    		--><li><a href="#" title="" class="twitter"> </a></li><!--
+    			    		--><li><a href="#" title="" class="pinterest"> </a></li>
+                    </ul>
+                    <ul class="copyright">
+                        <li>2015 UA КАТАЛОГ</li>
+                        <li>© Всі права захищені</li>
+                        <li>Всі ціни вказані в гривнях</li>
+                        <li>з урахуванням ПДВ</li>
+                        <li><a href="#" title="Правова інформація">Правова інформація</a></li>
+                    </ul>
+                </div><!--
+    			    --><div class="footer-column column-2">
+                    <span class="list-head">Виробникам</span>
+                    <ul>
+                        <li><a href="#" title="Про проект">Про проект</a></li>
+                        <li><a href="#" title="Зареєсрувати магазин">Зареєсрувати магазин</a></li>
+                        <li><a href="#" title="Розмістити рекламу">Розмістити рекламу</a></li>
+                        <li><a href="#" title="Розмістити акцію">Розмістити акцію</a></li>
+                        <li><a href="#" title="Зворотній з’язок">Зворотній з’язок</a></li>
+                    </ul>
+                </div><!--
+    			    --><div class="footer-column column-3">
+                    <span class="list-head">Користувачам</span>
+                    <ul>
+                        <li><a href="#" title="Власна колекція">Власна колекція</a></li>
+                        <li><a href="#" title="Правила коментування і написання відгуків">Правила коментування і написання відгуків</a></li>
+                        <li><a href="#" title="Зворотній зв'язок">Зворотній зв'язок</a></li>
+                        <li><a href="#" title="Допомога">Допомога</a></li>
+                        <li><a href="#" title="Мобільна версія">Мобільна версія</a></li>
+                    </ul>
+                </div><!--
+    			    --><div class="footer-column column-4">
+                    <span class="list-head">НОВИНИ ТА ПУБЛІКАЦІЇ</span>
+                    <ul class="list-news">
+                        <li><a href="#" title="	&quot;Зелене світло&quot;  у Європу: українські товари стають помітнішими на ринку ЄС">&quot;Зелене світло&quot;  у Європу: українські товари стають помітнішими на ринку ЄС</a></li>
+                        <li><a href="#" title="Росспоживнагляд почав масштабну перевірку українських товарів">Росспоживнагляд почав масштабну перевірку українських товарів</a></li>
+                        <li><a href="#" title="За останні 5 місяців Україна поставила в Крим товарів більш, ніж на $ 500 млн">За останні 5 місяців Україна поставила в Крим товарів більш, ніж на $ 500 млн</a></li>
+                        <li><a href="#" title="Огляд українського бренду">Огляд українського бренду</a></li>
+                        <li><a href="#" title="Києві пройде ярмарка український товарів">Києві пройде ярмарка український товарів</a></li>
+                        <li><a href="#" title="Українські іграшки мають свою філософію">Українські іграшки мають свою філософію</a></li>
+                        <li><a href="#" title="	&quot;Зелене світло&quot;  у Європу: українські товари стають помітнішими на ринку ЄС">	&quot;Зелене світло&quot;  у Європу: українські товари стають помітнішими на ринку ЄС</a></li>
+                        <li><a href="#" title="Росспоживнагляд почав масштабну перевірку українських товарів">Росспоживнагляд почав масштабну перевірку українських товарів</a></li>
+                    </ul>
+                    <span class="good-news">Ми приносимо ТІЛЬКИ ДОБРІ НОВИНИ</span>
+                    <span class="good-news-rss">Підпишись та дізнавайся першим</span>
+                    <form action="index.php" method="get">
+                        <input type="text" name="Email" placeholder="Email">
+                        <input type="submit" name="submit" value="Підписатись">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ views/layouts/_header.php
@@ -0,0 +1,53 @@
+<?php
+use yii\helpers\Html;
+?>
+<div class="header">
+    <div class="wrapper clearfix">
+        <div class="logo"><a href="#" title="logo"><img src="img/logo.png" alt="logo"></a>
+        </div>
+        <ul class="UAkatalog">
+            <li class="UAkatalog-link"><a href="#" title="Каталог">Каталог</a>
+                <ul class="sub-menu-lv1">
+                    <li class="hygiene"><a href="#" title="Засоби гігієни">Засоби гігієни</a>
+                        <ul class="sub-menu-lv2">
+                            <li class="personal-hygiene"><a href="#" title="Особиста гігієна">Особиста гігієна</a>
+                                <ul class="sub-menu-lv3">
+                                    <li><a href="#" title="Зубні пасти">Зубні пасти</a></li>
+                                    <li><a href="#" title="Шампуні">Шампуні</a></li>
+                                    <li><a href="#" title="Мило">Мило</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#" title="Для дому">Для дому</a></li>
+                            <li><a href="#" title="Для дітей">Для дітей</a></li>
+                            <li><a href="#" title="Жіноча гігієна">Жіноча гігієна</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#" title="Одяг">Одяг</a></li>
+                    <li><a href="#" title="Взуття">Взуття</a></li>
+                    <li><a href="#" title="Сумки">Сумки</a></li>
+                    <li><a href="#" title="Косметика">Косметика</a></li>
+                    <li><a href="#" title="Прикраси">Прикраси</a></li>
+                    <li><a href="#" title="Посуд">Посуд</a></li>
+                    <li><a href="#" title="Дитячі товари">Дитячі товари</a></li>
+                    <li><a href="#" title="Їжа">Їжа</a></li>
+                    <li><a href="#" title="Алкогольні напої">Алкогольні напої</a></li>
+                    <li><a href="#" title="Різне">Різне</a></li>
+                </ul>
+            </li>
+        </ul>
+        <ul class="main-menu">
+            <li><a href="#" title="Про нас">Про нас</a></li>
+            <li><a href="#" title="Події">Події</a></li>
+            <li><a href="#" title="Новини">Новини</a></li>
+            <li><a href="#" title="Блог">Блог</a></li>
+        </ul>
+        <div class="personal-info">
+            <a href="#" title="Привіт, Таня" class="login">Привіт, Таня</a>
+            <a href="#" title="Моя колекція" class="my-collection">Моя колекція</a>
+        </div>
+        <div class="language">
+            <a href="#" title="Укр" class="UA active">Укр</a>
+            <a href="#" title="Рос" class="RU">Рос</a>
+        </div>
+    </div>
+</div>
